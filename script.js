030cbf62b697d1f94a272db0fbca5aca46762430
Ansh <76505499+AnonymousTechDeveloper@users.noreply.github.com>
2023-09-10 14:03:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+function redirect(link) {
+    window.location.href = link;
+}
+
+const mainContainer = document.getElementById("content")
+const popupScreen = document.getElementById("popup-screen")
+const videoPlayer = document.getElementById("video-player")
+const profilePic = document.getElementById("profile-pic")
+const searchBar = document.getElementById("search-bar")
+
+searchBar.onkeyup = (event) => {
+    if (event.key === "Enter") {searchQuery()}
+}
+
+profilePic.onclick = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    window.location.replace("https://youtu.be/a3Z7zEc7AXQ")
+}
+
+videoList = [
+    {
+        title: "Day 0",
+        url: "https://Www.youtube.com/embed/6g3XQwLA_yE",
+        thumbUrl: "https://i.ytimg.com/vi/6g3XQwLA_yE/hq720.jpg"
+    },
+]
+
+const defaultVideoCardNumber = 9
+const rateOfCardGenerated = 6
+const numberOfVideoCards = videoList.length;
+renderedVideoCards = 0
+
+function videoCardRender(number) {
+    number = number + renderedVideoCards;
+    if (number > numberOfVideoCards) {number = numberOfVideoCards}
+    for (let i = renderedVideoCards; i < number; i++) {
+        mainContainer.innerHTML += `
+    <div class="video-card" onclick='launchVideoPlayer(${i})' title="${videoList[i].title}">
+        <div class="thumbnail" style="background-image: url('${videoList[i].thumbUrl}')"></div>
+    </div>`
+    }
+    renderedVideoCards = number;
+}
+
+function launchVideoPlayer(index) {
+    if (index === -1) {videoPlayer.src = "https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"}
+    else {videoPlayer.src = videoList[index].url}
+    popupScreen.style.display = "block"
+}
+
+function searchQuery() {
+    alert("We haven't started working on it yet lol")
+}
+
+videoCardRender(defaultVideoCardNumber)
